9a5d119822c86b971cfda66dc5bd01e24d8381f8
OleksiiA9 <vermiiashOleksii@gmail.com>
2017-07-13 17:00:50 +0300

    responsive 640

--- js/slider2.js
+++ js/slider2.js
@@ -1,47 +1,22 @@
 $(document).ready(function(){
   $('.sl2').slick({
-      //авто проктутка//
+      //авто проктутка
       //autoplay: true,
-      //скорость авто прокрутки 3с//
+      //скорость авто прокрутки 3с
       //autoplaySpeed: 500,
-      //скорость анимации//
+      //скорость анимации
       //speed: 1000,
-      //тип анимации : //linear, ease-out//
+      //тип анимации : //linear, ease-out
       cssEase: 'ease-in-out',
-      //центрирование//
-      //centerMode: true,//
-      //ширина боковых полос//
-      //centerPadding: '25px',//
-      //нижняя навагация//
-      //dots: true,
-      //отключение боковых стрелок для пролистования//
+      //отключение боковых стрелок для пролистования
       arrows: false,
-      //анимация изменения слайда, через тени//
-      //fade: true,//
-      //отмена пролистования мышкой(курсором), навигация только через стрелки и нижнюю навигацию//
-      //draggable: false,//
-      //отключение цикличности и зазор//
-      //infinite: false,//
-      //edgeFriction: '0.5',//
-      //пауза при наведении на слайд//
-      //pauseOnDotsHover: true,
-      //способ загрузки картинок, по умолчанию 'ondemand' -  сначало картинка прогружаеться потом идёт анимация, 'progressive' - //
-      //lazyLoad: 'ondemand',//
-      //направление слайдов, в html - сладеру нужно задать dir="rtl"//
-      //rtl: true,//
-      //вертикальное перелистование слайдов//
-      //vertical: true,//
-      //слайды идут в 2 строки//
-      //rows: 2,//
-      //столбцы//
-      //slidesPerRow: 2,//
-      //сколько слайдов видим одновременно//
+      //сколько слайдов видим одновременно
       slidesToShow: 4,
-      //перемотка сразу несколько слайдов//
+      //перемотка сразу несколько слайдов
       slidesToScroll: 1,
-      //zindex тоже что и в css z-index//
+      //zindex тоже что и в css z-index
       zIndex: 1000,
-      //респонсив при ширине экрана 768px и 480px//
+      //респонсив при ширине экрана 
 responsive: [
     {
         breakpoint: 1920,
@@ -79,8 +54,8 @@ responsive: [
             arrows: false,
             //centerMode: true,
             //centerPadding: '40px',
-            slidesToShow: 2,
-            slidesToScroll: 2,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             }
     },
     {
@@ -89,12 +64,12 @@ responsive: [
             arrows: false,
             //centerMode: true,
             //centerPadding: '40px',
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToShow: 2,
+            slidesToScroll: 2,
             }
     },
     {
-        breakpoint: 320,
+        breakpoint: 400,
             settings: {
             arrows: false,
             //centerMode: true,
